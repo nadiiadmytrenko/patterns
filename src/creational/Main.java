8f2c5d72f01d076406bb7db1e4a95aea7e01088b
nadiiadmytrenko <dmytrenko.nadiia@chnu.edu.ua>
2026-04-17 00:30:38 +0300

    Update Main.java

--- src/creational/Main.java
+++ src/creational/Main.java
@@ -1,4 +1,5 @@
 package creational;
+import structural.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -19,5 +20,34 @@ creator.create().open();
  WorkerRobot original = new WorkerRobot();
  WorkerRobot clone = (WorkerRobot) original.clone();
  clone.work();
+ System.out.println("\n 7. Adapter ");
+        // Створюємо адаптер, щоб підключити MicroUSB через звичайний USB
+        USB adapter = new Adapter(new MicroUSB());
+        adapter.connect();
+
+        System.out.println("\n 8. Bridge ");
+        Remote remote = new SimpleRemote(new TV());
+        remote.toggle();
+
+        System.out.println("\n 9. Composite ");
+        Composite root = new Composite();
+        root.add(new Leaf()); // Додаємо "файлик" у "папку"
+        root.show();
+
+        System.out.println("\n 10. Decorator ");
+        Coffee myCoffee = new MilkDecorator(new SimpleCoffee());
+        System.out.println(myCoffee.getDesc()); // Виведе: Кава з молоком
+
+        System.out.println("\n 11. Facade ");
+        CarFacade car = new CarFacade();
+        car.startCar(); // Одна кнопка запускає і двигун, і світло
+
+        System.out.println("\n 12. Flyweight ");
+        // Отримуємо тип дерева з кешу
+        TreeFactory.getType("Дуб").draw();
+
+        System.out.println("\n 13. Proxy ");
+        Image image = new ProxyImage();
+        image.display(); // Завантажить і покаже картинку
     }
 }
